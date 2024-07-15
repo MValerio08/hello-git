f78b414c7ee0a7e9752de1591b297496f6eece4d
MValerio08 <marcos88.ramos@gmail.com>
2024-07-15 19:36:41 -0400

    Se inserti comentarios con los comando mas usados

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,17 @@
 print("hello git!")
+
+
+
+# git config --global user.name "MValerio08" // asignar usuario al archivo
+# git config --global user.mail
+# git init // para iniciar el git del proyecto
+# git branch -m main // linea apara cambiar el nombre de la rama principal 
+# git status // muestra los archivos que se anadieron
+# git add hellogit.py // agrega los archivos seleccionados para hacer el seguimiento
+# git commit add -m "Este es mi primer commit" // agrega el commit para el archivo
+#
+# git log // muestra la creacion del archivo el usuario y el email de usuario que lo creo
+# git checkout <nombre del fichero> // no guarda los cambios realizados y muestra el fichero al comienzo
+
+# git add helogit.py
+# git commit "Se inserto comentarios con los comandos"
