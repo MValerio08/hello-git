8391749180948a9fedf07f353659c62be3df3c2f
MValerio08 <marcos88.ramos@gmail.com>
2024-07-16 13:26:59 -0400

    nuevos comandos registrados

--- hellogit2.py
+++ hellogit2.py
@@ -1,2 +1,9 @@
 print("hola desde hello git 2")
 #haciendo la prueba del commit  
+
+print("haciendo la prueba de diff")
+print("haciendo la prueba de diff1")
+print("haciendo la prueba de diff son tres prints que agregamos")
+
+# git dif // muestra los cambios realizados sin guardarlos 
+# git checkout HEAD
